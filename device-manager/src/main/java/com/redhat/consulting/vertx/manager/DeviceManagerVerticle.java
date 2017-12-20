0086a774ac01d92be48a250e01905d0dec33bcf0
David Sancho <dsanchor@redhat.com>
2017-12-20 18:42:37 +0100

    Added kubernetes client request.. for testing only

--- device-manager/src/main/java/com/redhat/consulting/vertx/manager/DeviceManagerVerticle.java
+++ device-manager/src/main/java/com/redhat/consulting/vertx/manager/DeviceManagerVerticle.java
@@ -1,7 +1,13 @@
 package com.redhat.consulting.vertx.manager;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.redhat.consulting.vertx.model.Device;
 import com.redhat.consulting.vertx.model.Room;
 
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.MessageConsumer;
 import io.vertx.core.json.Json;
@@ -13,6 +19,8 @@ public class DeviceManagerVerticle extends AbstractVerticle {
 	public static final String DEVICE_REGISTRATION_SERVICE_ADDRESS = "devices";
 
 	public static final String DEVICE_OPERATION_HEADER = "device-operation";
+	
+	public static final String HOMEPLAN_ID_HEADER = "homeplan-id";
 
 	public enum DeviceManagerOperation {
 		REGISTER, UNREGISTER
@@ -25,30 +33,54 @@ public class DeviceManagerVerticle extends AbstractVerticle {
 
 		MessageConsumer<String> consumer = vertx.eventBus().consumer(DEVICE_REGISTRATION_SERVICE_ADDRESS);
 		consumer.handler(message -> {
-			logger.info("Message received: " +message.body());
+			logger.info("Message received: " + message.body());
 			String operation = message.headers().get(DEVICE_OPERATION_HEADER);
 			if (operation != null && !"".equals(operation)) {
 				DeviceManagerOperation op = DeviceManagerOperation.valueOf(operation.toUpperCase());
+				String homeplanId = message.headers().get(HOMEPLAN_ID_HEADER);
 				switch (op) {
 				case REGISTER:
-					register(Json.decodeValue(message.body(), Room.class));
+					register(Json.decodeValue(message.body(), Room.class), homeplanId);
 					break;
 				case UNREGISTER:
-					unregister(Json.decodeValue(message.body(), Room.class));
+					unregister(Json.decodeValue(message.body(), Room.class), homeplanId);
 					break;
 				}
 			}
 		});
 	}
 
-	private void register(Room room) {
+	// FIXME use sync workers
+	private void register(Room room, String homeplanId) {
 		logger.info("Registering devices");
-
-		
+		OpenShiftClient client = new DefaultOpenShiftClient();
+		for (Device device : room.getDevices()) {
+			// TODO fix image reference.. I just put one for testing (it should be obtained from param/envvar
+			logger.info("Creating device: " + createName(homeplanId, room.getId(), device.getId()));
+			// create labels
+			Map<String, String> labels = new HashMap<>();
+			labels.put("homeplan", homeplanId);
+			labels.put("room", room.getId());
+			labels.put("device", device.getId());
+			// create DC
+			client.deploymentConfigs().createOrReplaceWithNew().withNewMetadata().withName(createName(homeplanId, room.getId(), device.getId()))
+					.withLabels(labels).endMetadata().withNewSpec().withReplicas(1).addNewTrigger()
+					.withType("ConfigChange").endTrigger().addToSelector("device", device.getId()).withNewTemplate()
+					.withNewMetadata().addToLabels("device", device.getId()).endMetadata().withNewSpec()
+					.addNewContainer().withName("device")
+					.withImage("docker-registry.default.svc:5000/"+client.getNamespace()+"/sensor:latest").endContainer()
+					.endSpec().endTemplate().endSpec().done();
+			logger.info("Device created");
+		}
 	}
-	
-	private void unregister(Room decodeValue) {
+
+	private String createName(String homeplanId, String room, String device) {
+		return homeplanId +"-"+room+"-"+device;
+	}
+
+	// FIXME use sync workers
+	private void unregister(Room decodeValue, String homeplanId) {
 		logger.info("Unregistering devices");
-		
+
 	}
 }
